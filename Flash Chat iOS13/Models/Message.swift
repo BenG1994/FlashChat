f5c8aad9191d4047609666b6ed05c28321b86755
BenG1994 <113778995+BenG1994@users.noreply.github.com>
2023-02-08 16:12:52 -0600

    Finished project

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Ben Gauger on 9/18/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+    
+    
+}
